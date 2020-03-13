ddf6e33bf40d94674d0f123ae269b3204c7f21c7
valentin1991 <valentin.semenov.1991@mail.ru>
2020-03-13 22:11:17 +0300

    homework14

--- js/index.js
+++ js/index.js
@@ -6,7 +6,6 @@ let objective = '';
 function addline(){
   document.getElementById('informmessage').style.display='block';
   setTimeout(function() {document.getElementById('informmessage').style.display='none'}, 1500);
-
   objective += "Задание"+ i + ": " + document.getElementById('object').value + "<br>";
   i += 1;
 }
@@ -14,5 +13,25 @@ function addline(){
 function showlist(){
   document.getElementById('list').style.display='block';
   document.getElementById('list').innerHTML = objective;
-
 }
+
+
+
+;(function($,undefined){
+  var j = 1;
+  let objectivejQuery = '';
+
+    $("button.addlinejQuery").on('click',function(){
+      $("#informmessagejquery").show();
+      setTimeout(function () {
+        $("#informmessagejquery").hide();
+      }, 1500)
+      objectivejQuery +="Задание"+ j + ": " +  $("#objectjQuery").val() + "<br>";
+      j += 1;
+    })
+
+    $("button.showlistjQuery").on('click',function(){
+      $("#listjquery").show();
+      $("#listjquery").html(objectivejQuery);
+    })
+})(jQuery)
