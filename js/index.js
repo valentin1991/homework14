050a2dcd37d37e565495fd712c3e9aeaa7a18dcf
valentin1991 <valentin.semenov.1991@mail.ru>
2020-03-13 15:53:46 +0300

    homework14

--- js/index.js
+++ js/index.js
@@ -1,5 +1,11 @@
 
 function showlist(){
-  document.getElementById('list').style.display='block';
-  setTimeout(function() {document.getElementById('list').style.display='none'}, 1500);
+  document.getElementById('informmessage').style.display='block';
+  setTimeout(function() {document.getElementById('informmessage').style.display='none'}, 1500);
+}
+
+function addline(){
+  var objective = document.getElementById('object').;
+  console.log(objective);
+  document.getElementById('list').innerHTML += objective + "<br>";
 }
